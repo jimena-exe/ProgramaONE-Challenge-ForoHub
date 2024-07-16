caee85929cfa3ad45f6807cb7b9aae8997871dfa
jimena-exe <jimena.alvarezva@amigo.edu.co>
2024-07-16 18:52:04 -0500

    Configuracion base del usuario

---
+++ src/main/java/ForoHub/challenge/domain/usuario/DatosAutenticacionUser.java
@@ -0,0 +1,5 @@
+package ForoHub.challenge.domain.usuario;
+
+//DTO
+public record DatosAutenticacionUser(String nombreUsuario, String clave) {
+}
---
+++ src/main/java/ForoHub/challenge/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,6 @@
+package ForoHub.challenge.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
