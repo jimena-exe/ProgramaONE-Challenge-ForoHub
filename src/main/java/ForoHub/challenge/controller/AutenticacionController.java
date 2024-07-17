bc3af8e53c0a22ab5d0166cb36b0ef4d9967bce3
jimena-exe <jimena.alvarezva@amigo.edu.co>
2024-07-16 20:45:27 -0500

    primera parte autenticacion usuario

--- src/main/java/ForoHub/challenge/controller/AutenticacionController.java
+++ src/main/java/ForoHub/challenge/controller/AutenticacionController.java
@@ -1,22 +1,31 @@
-//package ForoHub.challenge.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.core.token.TokenService;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/login")
-//public class AutenticacionController {
-//
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
+package ForoHub.challenge.controller;
+
+import ForoHub.challenge.domain.usuario.DatosAutenticacionUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutenticacionController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
 //    @Autowired
 //    private TokenService tokenService;
-//
-//    public void autenticarUsuario(){
-//
-//    }
-//}
+
+    @PostMapping
+    public ResponseEntity autenticarUsuario(DatosAutenticacionUser datosAutenticacionUser){
+        Authentication token = new UsernamePasswordAuthenticationToken(datosAutenticacionUser.nombreUsuario(),
+                datosAutenticacionUser.clave());
+        authenticationManager.authenticate(token);
+        return ResponseEntity.ok(token);
+    }
+}
